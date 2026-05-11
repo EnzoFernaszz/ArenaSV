2568a9344927b83e63aff77431fe014682126fd0
GuilhermeMAzevedo <guilhermemazevedo2007@gmail.com>
2026-05-11 17:10:18 -0300

    Botão skip

--- Scenes/second_drawn.gd
+++ Scenes/second_drawn.gd
@@ -1,5 +1,13 @@
 extends Control
 
+func _ready() -> void:
+	var buttonSkip = $PanelContainer/ScoreLabel.get_node("SkipButton")
+	
+	if buttonSkip:
+		buttonSkip.pressed.connect(skip_to_game)
+
+func skip_to_game():
+	get_tree().change_scene_to_file("res://Scenes/survivors_game.tscn")
 
 func _on_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://Scenes/third_drawn.tscn")
--- Scenes/story_screen.gd
+++ Scenes/story_screen.gd
@@ -1,7 +1,14 @@
 extends Control
 
 func _ready():
+	var buttonSkip = $PanelContainer/ScoreLabel.get_node("SkipButton")
 	GerenciadorAudio.tocar_musica_historia()
+	
+	if buttonSkip:
+		buttonSkip.pressed.connect(skip_to_game)
+
+func skip_to_game():
+	get_tree().change_scene_to_file("res://Scenes/survivors_game.tscn")
 
 func _on_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://Scenes/second_drawn.tscn")
