f723e4084df72291a731e3a16a55354dad84f2f9
SamuelL0pes-dev <sg273041@gmail.com>
2026-05-12 16:41:50 -0300

    ATUALIZAÇÃO DE TELAS

---
+++ Scenes/controlPause.gd
@@ -0,0 +1,5 @@
+extends Control
+
+func _on_button_continuar_pressed():
+	get_tree().paused = false
+	visible = false
--- Scenes/pause.gd
+++ Scenes/pause.gd
@@ -1,8 +1,12 @@
 extends Node
 
+@export var pause_menu: Control
+
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
+	pause_menu.visible = false
 
 func _input(event):
 	if event.is_action_pressed("pause"):
 		get_tree().paused = !get_tree().paused
+		pause_menu.visible = get_tree().paused
